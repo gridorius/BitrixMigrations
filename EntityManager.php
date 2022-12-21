aeddb1611dba8ff3ec36924f7b4f73eb31b5e89a
gridorius <gridorius@yandex.ru>
2022-12-21 22:33:03 +0300

    initial

---
+++ EntityManager.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Bitrix\Migration;
+
+use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Iblock\PropertyEnumerationTable;
+use Bitrix\Main\Entity\AddResult;
+use Bitrix\Main\Loader;
+use const Bitrix\Main\UserFieldLangTable;
+
+class EntityManager
+{
+    protected $ufManager;
+    protected $ufEnumsManager;
+    protected $iblockPropertyManager;
+    protected $iblockPropertyEnumsManager;
+
+    public function __construct()
+    {
+        Loader::includeModule('highloadblock');
+        $this->ufManager = new \CUserTypeEntity();
+        $this->ufEnumsManager = new \CUserFieldEnum();
+        $this->iblockPropertyManager = new \CIBlockProperty();
+        $this->iblockPropertyEnumsManager = new \CIBlockPropertyEnum();
+    }
+
+    public function AddHighload($fields): AddResult
+    {
+        return HighloadBlockTable::add($fields);
+    }
+
+    public function addHighloads($data)
+    {
+        foreach ($data as $fields) {
+            $this->AddHighload($fields);
+        }
+    }
+
+    public function addUserField($fields): int
+    {
+        return $this->ufManager->Add($fields);
+    }
+
+    public function addUserFields($data)
+    {
+        foreach ($data as $fields) {
+            $this->addUserField($fields);
+        }
+    }
+
+    public function addUserFieldLang($fields)
+    {
+        return UserFieldLangTable::add($fields);
+    }
+
+    public function addUserFieldsLangs($data){
+        foreach ($data as $fields) {
+            $this->addUserFieldLang($fields);
+        }
+    }
+
+    public function addUserFieldEnums($fieldId, $values){
+        return $this->ufEnumsManager->SetEnumValues($fieldId, $values);
+    }
+
+    public function addUerFieldsEnums($data){
+        $fields = [];
+
+        foreach ($data as $row){
+            $fields[$row['USER_FIELD_ID']][$row['ID']] = $row;
+        }
+
+        foreach ($fields as $fieldId => $values){
+            $this->addUserFieldEnums($fieldId, $values);
+        }
+    }
+
+    public function addIblockProperty($fields){
+        return $this->iblockPropertyManager->Add($fields);
+    }
+
+    public function addIblockPropertyEmum($fields){
+        return PropertyEnumerationTable::add($fields);
+    }
+
+    public function addIblockProperties($data){
+        foreach ($data as $fields) {
+            $this->addIblockProperty($fields);
+        }
+    }
+
+    public function addIblockPropertiesEnums($data){
+        foreach ($data as $fields) {
+            $this->addIblockPropertyEmum($fields);
+        }
+    }
+}
---
+++ MigrationBuilder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Bitrix\Migration;
+
+use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Main\Loader;
+use Bitrix\Main\UserFieldLangTable;
+
+class MigrationBuilder
+{
+    protected $configuration = [];
+
+    public function __construct()
+    {
+        Loader::includeModule('highloadblock');
+    }
+
+    function buildFromDB()
+    {
+        $highloads = HighloadBlockTable::getList()->fetchAll();
+        $userFieldsLang = UserFieldLangTable::getList()->fetchAll();
+        $userFields = $this->collectResultValues(\CUserTypeEntity::GetList());
+        $userFieldsEnums = $this->collectResultValues(\CUserFieldEnum::GetList());
+        $iblockProperties = $this->collectResultValues(\CIBlockProperty::GetList());
+        $iblockPropertiesEnums = $this->collectResultValues(\CIBlockPropertyEnum::GetList());
+
+        return [
+            'Highloads' => $highloads,
+            'UserFields' => $userFields,
+            'UserFieldsLang' => $userFieldsLang,
+            'UserFieldsEnums' => $userFieldsEnums,
+            'IblockProperties' => $iblockProperties,
+            'IblockPropertiesEnums' => $iblockPropertiesEnums,
+        ];
+    }
+
+    protected function collectResultValues($result){
+        $items = [];
+        while($item = $result->Fetch()){
+            $items[] = $item;
+        }
+        return $items;
+    }
+
+    public function generateMigrationString(){
+        $data = $this->buildFromDB();
+        $result = '<?php'.PHP_EOL.'use Bitrix\DataMover\Migration;'
+            .PHP_EOL.PHP_EOL
+            .'$migration = new Migration()'.PHP_EOL;
+
+        foreach ($data as $dataKey => $dataItem){
+            $result.= $this->buildData($dataKey, $dataItem);
+        }
+
+        return $result;
+    }
+
+    public function saveToFile($path){
+        file_put_contents($path, $this->generateMigrationString());
+    }
+
+    protected function buildData($dataKey, $data){
+        $methodName = 'build'.$dataKey.'Data';
+        if(method_exists($this, $methodName)){
+            return call_user_func(
+                    [$this, $methodName],
+                    $this->makeDataString($data)
+                ).PHP_EOL.PHP_EOL;
+        }
+        return '';
+    }
+
+    protected function makeDataString($data){
+        $export = var_export($data, true);
+        $patterns = [
+            "/array \(/" => '[',
+            "/^([ ]*)\)(,?)$/m" => '$1]$2',
+            "/=>[ ]?\n[ ]+\[/" => '=> [',
+            "/([ ]*)(\'[^\']+\') => ([\[\'])/" => '$1$2 => $3',
+        ];
+        return preg_replace(array_keys($patterns), array_values($patterns), $export);
+    }
+
+    protected function buildHighloadsData($dataString){
+        return "\$migration->addHighloads({$dataString});";
+    }
+
+    protected function buildUserFieldsData($dataString){
+        return "\$migration->addUserFields({$dataString});";
+    }
+
+    protected function buildUserFieldsLangData($dataString){
+        return "\$migration->addUserFieldsLangs({$dataString});";
+    }
+
+    protected function buildUserFieldsEnumsData($dataString){
+        return "\$migration->addUerFieldsEnums({$dataString});";
+    }
+
+    protected function buildIblockPropertiesData($dataString){
+        return "\$migration->addIblockProperties({$dataString});";
+    }
+
+    protected function buildIblockPropertiesEnumsData($dataString){
+        return "\$migration->addIblockPropertiesEnums({$dataString});";
+    }
+}
